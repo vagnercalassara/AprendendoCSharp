84d95ab13543d2a49ab3a05132266abc7ee2d041
Vagner Calassara <vagnercalassara@gmail.com>
2023-12-14 10:34:37 -0300

    Projeto de Incialização

---
+++ 5-CaracteresETexto/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Executando o projeto 5 - Caracteres e textos");
+
+//character
+char primeiraLetra = 'a';
+Console.WriteLine(primeiraLetra);
+
+primeiraLetra = (char)65;
+Console.WriteLine(primeiraLetra);
+
+primeiraLetra = (char)(primeiraLetra + 1);
+Console.WriteLine(primeiraLetra);
+
+primeiraLetra &= primeiraLetra;
+Console.WriteLine(primeiraLetra);
+
+string titulo = "Um titulo qualquer " + 2023;
+Console.WriteLine(titulo);
+
+string cursos = "- .Net \n" +
+    "- Java \n" +
+    "- JavaScript";
+
+Console.WriteLine(cursos);
