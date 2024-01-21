ecf682cb5bb19c6192630ac319b476a7adea817e
vagnercalassara <vagnercalassara@gmail.com>
2024-01-21 14:48:03 -0300

    Alterando a mensagem

--- 8-Condicionais2/Program.cs
+++ 8-Condicionais2/Program.cs
@@ -1,13 +1,11 @@
-﻿// Alteração Feita Pelo Vagner...
-
-var data = DateTime.Today;
+﻿var data = DateTime.Today;
 Console.WriteLine($"Executando projeto 8 - Condicionais, inicnio em: {data}");
 
 int? idade = 19;
 int? quatidadePessoas = 2;
 bool acompanhado = quatidadePessoas >= 2;
 
-if (idade is not null && idade >= 18 || acompanhado)
-    Console.WriteLine("Joao tem 18 anos");
+if ((idade is not null && idade >= 18) || acompanhado)
+    Console.WriteLine($"Joao tem 18 anos");
 else
-    Console.WriteLine("Joao não tem 18 anos. Joao tem " + idade);
+    Console.WriteLine($"Joao não tem 18 anos. Joao tem {idade}");
