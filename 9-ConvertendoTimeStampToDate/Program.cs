8d66dd2b78bd4b8ca9ad7acb74b613715ed92701
vagnercalassara <vagnercalassara@gmail.com>
2024-01-28 19:57:01 -0300

    Convertendo TimeStamp para Data

---
+++ 9-ConvertendoTimeStampToDate/Program.cs
@@ -0,0 +1,7 @@
+﻿long timestamp = 1706343583;
+DateTime dataHora = DateTimeOffset.FromUnixTimeSeconds(1706343583).UtcDateTime;
+var quantHora = dataHora.AddHours(-12);
+Console.WriteLine($"Valor do TimeStamp:  {timestamp}");
+Console.WriteLine($"Convertendo para Data:  {quantHora.ToString("dd/MM/yyyy HH:mm:ss")}");
+
+
